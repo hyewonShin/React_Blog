4fa1fc9bb1a216c5396bb0aa3460e5b91c6f956a
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-02-03 20:17:35 +0900

    userState(prevState)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { useState } from "react";
-
+// 리렌더링  : 함수가 한 번 더 실행된다는 뜻.
 function App() {
-  const [number, setNumber] = useState(3);
-
+  console.log("render");
+  const [number, setNumber] = useState(1);
   const double = () => {
-    const doubledNumber = number * 2;
-    setNumber(doubledNumber);
-    console.log(doubledNumber);
+    setNumber((prevState) => prevState * 2);
+    setNumber((prevState) => prevState * 2);
   };
 
   return (
