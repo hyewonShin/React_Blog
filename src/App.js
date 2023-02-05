0809f0cf872d76bcbfc9d3d4ddbb42044d7e4090
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-02-05 18:10:53 +0900

    routes 파일 만들기

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import CreatePage from "./pages/CreatePage";
-import EditPage from "./pages/EditPage";
-import HomePage from "./pages/HomePage";
-import ListPage from "./pages/ListPage";
+import routes from "./routes";
 
 function App() {
   return (
     <Router>
       <NavBar />
+
       <div className="container">
         <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-          <Route path="/blogs" exact>
-            <ListPage />
-          </Route>
-          <Route path="/blogs/create" exact>
-            <CreatePage />
-          </Route>
-          <Route path="/blogs/edit" exact>
-            <EditPage />
-          </Route>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}
+                exact
+                path={route.path}
+                component={route.component}
+              />
+            );
+          })}
         </Switch>
       </div>
     </Router>
