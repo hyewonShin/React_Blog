7226d6e23db0492e3ddd2b20d2ae90ebd79d4a2f
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-02-05 16:19:28 +0900

    React Router 설치

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   const [title, setTitle] = useState("");
@@ -12,32 +13,43 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <div className="mb-3">
-        <label className="form-lable">Title</label>
-        <input
-          className="form-control"
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        />
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/blogs">Blogs</Link>
       </div>
-      <div className="mb-3">
-        <label className="form-lable">Body</label>
-        <textarea
-          className="form-control"
-          value={body}
-          onChange={(e) => {
-            setBody(e.target.value);
-          }}
-          rows="5"
-        />
-      </div>
-      <button className="btn btn-primary" onClick={onSubmit}>
-        Post
-      </button>
-    </div>
+      <Switch>
+        <Route path="/">Home Page</Route>
+        <Route path="/blogs">
+          <div className="container">
+            <div className="mb-3">
+              <label className="form-lable">Title</label>
+              <input
+                className="form-control"
+                value={title}
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-lable">Body</label>
+              <textarea
+                className="form-control"
+                value={body}
+                onChange={(e) => {
+                  setBody(e.target.value);
+                }}
+                rows="5"
+              />
+            </div>
+            <button className="btn btn-primary" onClick={onSubmit}>
+              Post
+            </button>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
