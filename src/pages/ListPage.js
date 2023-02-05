2d1cfe9ba8a93320e620a1f2f6b59d12686f4647
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-02-05 20:10:07 +0900

    생성 버튼

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Card from "../components/Card";
+import { Link } from "react-router-dom";
 
 const ListPage = () => {
   const [posts, setPosts] = useState([]);
@@ -18,7 +19,14 @@ const ListPage = () => {
 
   return (
     <div>
-      <h1>Blogs</h1>
+      <div className="d-flex justify-content-between">
+        <h1>Blogs</h1>
+        <div>
+          <Link to="/blogs/create" className="btn btn-success">
+            create New
+          </Link>
+        </div>
+      </div>
       {posts.map((post) => {
         return (
           <Card key={post.id} title={post.title} />
