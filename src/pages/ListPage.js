ebcc1bf1b0ce32209ac104b3349fca3730f99cae
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-02-08 21:15:23 +0900

    Loading Spinner

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -2,15 +2,18 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Card from "../components/Card";
 import { Link, useHistory } from "react-router-dom";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const ListPage = () => {
   const history = useHistory();
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getPosts = () => {
     axios.get("http://localhost:3001/posts").then((res) => {
       console.log(res.data);
       setPosts(res.data);
+      setLoading(false);
     });
   };
 
@@ -26,6 +29,35 @@ const ListPage = () => {
     getPosts();
   }, []);
 
+  const renderBlogList = () => {
+    if (loading) {
+      <LoadingSpinner />;
+    }
+
+    if (posts.length === 0) {
+      return <div>"No blog posts found"</div>;
+    }
+
+    return posts.map((post) => {
+      return (
+        <Card
+          key={post.id}
+          title={post.title}
+          onClick={() => history.push("/blogs/edit")}
+        >
+          <div>
+            <button
+              className="btn btn-danger btn-sm"
+              onClick={(e) => delteBlog(e, post.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </Card>
+      );
+    });
+  };
+
   return (
     <div>
       <div className="d-flex justify-content-between">
@@ -36,26 +68,7 @@ const ListPage = () => {
           </Link>
         </div>
       </div>
-      {posts.length > 0
-        ? posts.map((post) => {
-            return (
-              <Card
-                key={post.id}
-                title={post.title}
-                onClick={() => history.push("/blogs/edit")}
-              >
-                <div>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={(e) => delteBlog(e, post.id)}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </Card>
-            );
-          })
-        : "No blog posts found"}
+      {renderBlogList()}
     </div>
   );
 };
