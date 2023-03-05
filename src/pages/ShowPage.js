e6bf7124d41770bbfecda6ae1734a5206e51e390
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-03-05 16:19:41 +0900

    의존성 배열

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -2,11 +2,13 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import LoadingSpinner from "../components/LoadingSpinner";
+import { useHistory } from "react-router-dom";
 
 const ShowPage = () => {
   const { id } = useParams();
   const [post, setPost] = useState(null);
   const [loading, setLoding] = useState(true);
+  const history = useHistory();
 
   const getPost = (id) => {
     axios.get(`http://localhost:3001/posts/${id}`).then((res) => {
@@ -16,8 +18,9 @@ const ShowPage = () => {
   };
 
   useEffect(() => {
+    console.log("hello");
     getPost(id);
-  }, []);
+  }, [id]);
 
   if (loading) {
     return <LoadingSpinner />;
@@ -27,6 +30,7 @@ const ShowPage = () => {
     <div>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
+      <button onClick={() => history.push("/blogs/2")}>Click</button>
     </div>
   );
 };
