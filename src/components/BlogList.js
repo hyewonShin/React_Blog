ae85adcdc988c048116f49b6d3ab48b37ee2d726
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-03-05 21:29:29 +0900

    admin페이지 route추가 및 BlogList 컴포넌트 생성

---
+++ src/components/BlogList.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Card from "../components/Card";
+import { Link, useHistory } from "react-router-dom";
+import LoadingSpinner from "../components/LoadingSpinner";
+
+const BlogList = () => {
+  const history = useHistory();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getPosts = () => {
+    axios.get("http://localhost:3001/posts").then((res) => {
+      setPosts(res.data);
+      setLoading(false);
+    });
+  };
+
+  const delteBlog = (e, id) => {
+    e.stopPropagation();
+    console.log("delete blog");
+    axios.delete(`http://localhost:3001/posts/${id}`).then(() => {
+      setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
+    });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  if (loading) {
+    <LoadingSpinner />;
+  }
+
+  if (posts.length === 0) {
+    return <div>"No blog posts found"</div>;
+  }
+
+  return posts
+    .filter((post) => {
+      return post.publish;
+    })
+    .map((post) => {
+      return (
+        <Card
+          key={post.id}
+          title={post.title}
+          onClick={() => history.push(`/blogs/${post.id}`)}
+        >
+          <div>
+            <button
+              className="btn btn-danger btn-sm"
+              onClick={(e) => delteBlog(e, post.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </Card>
+      );
+    });
+};
+
+export default BlogList;
