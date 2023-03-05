c86526688fb90f7f2899e12bee2e16d08307705e
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-03-05 16:06:41 +0900

    블로그db에서 받아와서 화면에 보여주기

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -1,9 +1,33 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const ShowPage = () => {
   const { id } = useParams();
-  console.log("id", id);
-  return <div>Show Page</div>;
-};
+  const [post, setPost] = useState(null);
+  const [loading, setLoding] = useState(true);
 
+  const getPost = (id) => {
+    axios.get(`http://localhost:3001/posts/${id}`).then((res) => {
+      setPost(res.data);
+      setLoding(false);
+    });
+  };
+
+  useEffect(() => {
+    getPost(id);
+  }, []);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
 export default ShowPage;
