8bc60df6a5035cfba02af4a7fe8045e96a6b2f75
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-03-15 21:34:57 +0900

    검색기능 1

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -16,6 +16,7 @@ const BlogList = ({ isAdmin }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [numberOfPosts, setNumberOfPosts] = useState(0);
   const [numberOfPages, setNumberOfPages] = useState(0);
+  const [searchText, setSearchText] = useState("");
   const limit = 1;
 
   useEffect(() => {
@@ -33,6 +34,7 @@ const BlogList = ({ isAdmin }) => {
       _limit: limit,
       _sort: "id",
       _order: "desc",
+      title_like: searchText,
     };
 
     if (!isAdmin) {
@@ -49,7 +51,7 @@ const BlogList = ({ isAdmin }) => {
           setPosts(res.data);
           setLoading(false);
         },
-        [isAdmin]
+        [isAdmin, searchText]
       );
   });
 
@@ -68,10 +70,6 @@ const BlogList = ({ isAdmin }) => {
 
   if (loading) {
     <LoadingSpinner />;
-  }
-
-  if (posts.length === 0) {
-    return <div>"No blog posts found"</div>;
   }
 
   const renderBlogList = () => {
@@ -97,15 +95,34 @@ const BlogList = ({ isAdmin }) => {
     });
   };
 
+  const onSearch = () => {
+    getPosts(1);
+  };
+
   return (
     <div>
-      {renderBlogList()}
-      {numberOfPages > 1 && (
-        <Pagination
-          currentPage={currentPage}
-          numberOfPages={numberOfPages}
-          onClick={onClickPageButton}
-        />
+      <input
+        type="text"
+        placeholder="Search..."
+        className="form-control"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        onKeyUp={onSearch}
+      />
+      <hr />
+      {posts.length === 0 ? (
+        <div>"No blog posts found"</div>
+      ) : (
+        <>
+          {renderBlogList()}
+          {numberOfPages > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              numberOfPages={numberOfPages}
+              onClick={onClickPageButton}
+            />
+          )}
+        </>
       )}
     </div>
   );
