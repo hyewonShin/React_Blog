93e25cd32834e44ce56078693c454bace30c7c23
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-04-09 21:28:52 +0900

    auth redux slice 만들기

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import toastReducer from "./toastSlice";
+import authReducer from "./authSlice";
 
 export default configureStore({
   reducer: {
     toast: toastReducer,
+    auth: authReducer,
   },
 });
