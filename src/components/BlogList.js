b9f56b0729c44b640b10b676e172ab1314eeb409
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-05-07 21:33:50 +0900

    블로그 삭제 후 페이지 1로 이동

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -77,7 +77,8 @@ const BlogList = ({ isAdmin }) => {
     axios
       .delete(`http://localhost:3001/posts/${id}`)
       .then(() => {
-        setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
+        // setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
+        getPosts(1);
         addToast({
           text: "Successfully deleted",
           type: "success",
